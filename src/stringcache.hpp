5a75951ad5d606a3f4e01576352bca176dd8709c
TheJokr <leo@dashsec.com>
2019-11-30 23:12:02 +0100

    Added bounded-size character name cache

---
+++ src/stringcache.hpp
@@ -0,0 +1,79 @@
+/*
+ * chatload: Log reader to collect EVE Online character names
+ * Copyright (C) 2015-2019  Leo Blöcher
+ *
+ * This file is part of chatload-client.
+ *
+ * chatload-client is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * chatload-client is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with chatload-client.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+// Header guard
+#pragma once
+#ifndef CHATLOAD_STRINGCACHE_H
+#define CHATLOAD_STRINGCACHE_H
+
+
+// C headers
+#include <cstdint>
+#include <climits>
+
+// Containers
+#include <string>
+#include <vector>
+
+// Utility
+#include <type_traits>
+
+// xxHash3
+#include <xxh3.h>
+
+// chatload components
+#include "common.hpp"
+
+namespace chatload {
+// Inspired by https://cs.stackexchange.com/a/24122
+template<std::size_t index_bits, typename T = std::uint_least32_t>
+class string_cache {
+private:
+    static_assert(std::is_unsigned<T>::value, "Value (T) must be an unsigned arithmetic type");
+    static_assert((index_bits + sizeof(T) * CHAR_BIT) <= 64, "Index and value (T) may not exceed 64 bits");
+    std::vector<T> cache;
+
+public:
+    // Default initialization (all 0s) should be fine, since a collision is unlikely
+    string_cache() : cache(1ull << index_bits) {};
+
+    // Returns whether the key was added or not, i.e., whether the key is new or not
+    inline bool add_if_absent(const std::u16string& key) noexcept {
+        constexpr std::size_t full_mask = (1ull << (index_bits + sizeof(T) * CHAR_BIT)) - 1;
+        constexpr std::size_t idx_mask = (1ull << index_bits) - 1;
+        constexpr std::size_t value_mask = full_mask ^ idx_mask;
+
+        // xxHash takes size in bytes
+        XXH64_hash_t hash = XXH3_64bits(key.data(), sizeof(std::u16string::value_type) * key.size());
+        std::size_t idx = hash & idx_mask;
+        T val = (hash & value_mask) >> index_bits;
+
+        if (this->cache[idx] == val) {
+            return false;
+        } else {
+            this->cache[idx] = val;
+            return true;
+        }
+    };
+};
+}  // namespace chatload
+
+
+#endif  // CHATLOAD_STRINGCACHE_H
